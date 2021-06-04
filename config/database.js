5ef97cbb8276e759b456184ec626aa0f44564ac0
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-06-04 13:06:15 +0530

    Refactor: service file by using ternary operator

---
+++ config/database.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+/*
+ * function to connect mongoose database 
+ * @returns connection
+ */
+function dbconnect(){
+
+    mongoose.promise;
+    mongoose.connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+    });
+
+    return mongoose.connection
+    .once('open', () => console.log('Mongo database Connected'))
+    .on('error', (error)=> {
+        console.log("Eroor found",error)
+    });
+}
+
+module.exports=dbconnect;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dbconnect = require('./config/database.js');
+require("dotenv").config();
+// create express app
+const app = express();
+
+//Connect to DB
+dbconnect();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(express.json())     
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EmployeePayRoll application "});
+});
+
+// Require Notes routes
+require('./app/routers/employee')(app);
+
+// listen for requests
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("Server is listening on port "+process.env.SERVER_PORT);
+});
