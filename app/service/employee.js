9b1ac0450ef4bd86f39fbbc279eaf7c4d8e1fcbb
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-06-04 13:45:41 +0530

    refactor: controller files

--- app/service/employee.js
+++ app/service/employee.js
@@ -20,7 +20,6 @@ class UserService {
         });
     }
 
-
     findDataId = (employeObjectId, callback) => {
         employeeModel.findDataId(employeObjectId, (error, data) => {
             return (error) ? callback(error, null) : callback(null, data);
